d8275c31df8a8f56a10e8c5463f72aee2c5ce830
KHOULOUD BELAAZI <khouloud.balaazi@vernicolor.com>
2026-01-04 23:15:21 +0100

    feat: connect config server to git repo for centralized configuration

---
+++ backend/config-server/src/test/java/com/smartgreenhouse/itbs/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartgreenhouse.itbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
