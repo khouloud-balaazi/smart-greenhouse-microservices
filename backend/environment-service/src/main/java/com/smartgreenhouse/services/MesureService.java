4c839d700e722998c5e24acd988624d8228133e2
KHOULOUD BELAAZI <khouloud.balaazi@vernicolor.com>
2026-01-05 20:56:46 +0100

    feat(environment): add service layer for parameters and measurements

--- backend/environment-service/src/main/java/com/smartgreenhouse/services/MesureService.java
+++ backend/environment-service/src/main/java/com/smartgreenhouse/services/MesureService.java
@@ -1,5 +1,37 @@
 package com.smartgreenhouse.services;
 
-public class MesureService {
+import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.smartgreenhouse.Enum.ParametreType;
+import com.smartgreenhouse.entities.Mesure;
+import com.smartgreenhouse.entities.Parametre;
+import com.smartgreenhouse.repositories.MesureRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MesureService {
+	private final MesureRepository mesureRepository;
+    private final ParametreService parametreService;
+
+    public Mesure enregistrerMesure(ParametreType type, Double valeur) {
+
+        Parametre parametre = parametreService.getByType(type);
+
+        Mesure mesure = Mesure.builder()
+                .parametre(parametre)
+                .valeur(valeur)
+                .dateMesure(LocalDateTime.now())
+                .build();
+
+        return mesureRepository.save(mesure);
+    }
+
+    public List<Mesure> getMesuresByParametre(Long parametreId) {
+        return mesureRepository.findByParametreId(parametreId);
+    }
 }
