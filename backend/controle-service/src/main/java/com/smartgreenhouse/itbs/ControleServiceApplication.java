b603a23f72a8e97882ddb32f61369401456a33ff
KHOULOUD BELAAZI <khouloud.balaazi@vernicolor.com>
2026-01-11 16:22:50 +0100

    feat: add controllers, update main class and local config for controle-service

--- backend/controle-service/src/main/java/com/smartgreenhouse/itbs/ControleServiceApplication.java
+++ backend/controle-service/src/main/java/com/smartgreenhouse/itbs/ControleServiceApplication.java
@@ -2,8 +2,16 @@ package com.smartgreenhouse.itbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.smartgreenhouse")
+@EnableJpaRepositories("com.smartgreenhouse.repositories")
+@EntityScan("com.smartgreenhouse.entities") 
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ControleServiceApplication {
 
 	public static void main(String[] args) {
